dbc8d7589ddb97aa69bd0dd336b7b85425932996
kareemelnaghy <kareemelnaghy@aucegypt.edu>
2025-04-01 02:07:09 +0200

    added os_name getter (experimenting)

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,15 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn os_name(name: &str) -> String {
+    let info = os_info::get();
+    format!("{} {}", info.os_type(), info.version())
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![os_name])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
