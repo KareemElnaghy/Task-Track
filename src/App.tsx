e47c48022d00c2486f127b8efaaf757f2c6268d5
kareemelnaghy <kareemelnaghy@aucegypt.edu>
2025-04-04 00:56:40 +0200

    Added CMD to retrieve a list of processes

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,49 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  const [osName, setOSName] = useState("");
+  const [processes, setProcesses] = useState([]);
 
-  async function greet() {
+  async function getOsName() {
     // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("os_name", { name }));
+    setOSName(await invoke("os_name"));
+  }
+
+  async function getProcessesList(){
+    
+    const processes = await invoke("get_processes");
+    setProcesses(processes);
   }
 
   return (
     <main className="container">
-      <h1>Welcome to Tauri + React</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+      <h1>Getting OS Name</h1>
 
       <form
         className="row"
         onSubmit={(e) => {
           e.preventDefault();
-          greet();
+          getOsName();
+          getProcessesList();
         }}
       >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
+        <button type="submit">Yalla</button>
       </form>
-      <p>{greetMsg}</p>
+      <p>{osName}</p>
+      <div className="processes">
+        <h2>Processes</h2>
+        <ul>
+          {processes.map((process) => (
+            <li key={process.pid}>
+              <p>{process.name}</p>
+              <p>{process.pid}</p>
+              <p>{process.status}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
     </main>
   );
 }
