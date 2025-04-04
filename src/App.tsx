9fb8442a75ff3853ace8f85ee8d5236d13cb222f
FaridaBey <farida.bey@aucegypt.edu>
2025-04-05 00:28:42 +0200

    support for tabs in gui / refactoring overall apearance

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { useState } from "react";
+import { TbReload } from "react-icons/tb";
 import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 
 function App() {
   const [osName, setOSName] = useState("");
   const [processes, setProcesses] = useState([]);
+  const [hasFetched, setHasFetched] = useState(false);
+  const [activeTab, setActiveTab] = useState("processes");
+  const handleTabClick = (tab) => {
+    setActiveTab(tab);
+  };
 
   async function getOsName() {
     // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
@@ -28,6 +34,13 @@ function App() {
     await invoke("resume_process", { pid });
     getProcessesList();
   }
+  // async function handleTabClick(tab) {
+  //   setActiveTab(tab);
+  //   if (tab === "resources") {
+  //     //show Monitoring resources window
+  //   }
+  // }
+
   // Actions on all processes
   // async function handleSuspendAll() {
   //   await invoke("suspend_all_processes");
@@ -44,22 +57,52 @@ function App() {
 
   return (
     <main className="container">
-      <h1>Getting OS Name</h1>
+      {!hasFetched ? (
+        <>
+          <h1>Getting OS Name</h1>
 
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          getOsName();
-          getProcessesList();
-        }}
-      >
-        <button className="start">Yalla</button>
-      </form>
+          <form
+            className="row"
+            onSubmit={(e) => {
+              e.preventDefault();
+              getOsName();
+              getProcessesList();
+              setHasFetched(true);
+            }}
+          >
+            <button className="start">Yalla</button>
+          </form>
+        </>
+      ) : (
+        <div className="processes">
+          <button
+            className="relaod"
+            onClick={() => {
+              getOsName();
+              getProcessesList();
+            }}
+          >
+            <TbReload />
+          </button>
+        </div>
+      )}
       <p className="os-name">Operating System: {osName}</p>
-
+      <div className="tabs">
+        <div
+          className={`tab ${activeTab === "processes" ? "active" : ""}`}
+          onClick={() => handleTabClick("processes")}
+        >
+          Processes
+        </div>
+        <div
+          className={`tab ${activeTab === "resources" ? "active" : ""}`}
+          onClick={() => handleTabClick("resources")}
+        >
+          Resources
+        </div>
+        {/* Add more tabs as needed */}
+      </div>
       <div className="processes">
-        <h2>Processes</h2>
         <table>
           <thead>
             <tr>
