9226edba6bdcc93c219204c13b32d6f2bb41765d
kareemelnaghy <kareemelnaghy@aucegypt.edu>
2025-04-11 04:17:28 +0200

    expanded sorting logic

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -2,14 +2,14 @@
 use serde::{Serialize, Deserialize};
 use sysinfo::{System, Pid};
     // , ProcessExt, SystemExt};
-use std::{os::unix::process, process::Command};
+use std::{process::Command};
 
 #[derive(Serialize, Deserialize)]
 struct ProcessInfo { // struct to hold process information
     pid: u32,
     name: String,   //TODO: add more fields to this struct
     status: String,
-    cpu_usage: f32,
+    cpu_usage: f32, // TODO: add CPU usage
     // mem_usage: f32,
 }
 
@@ -20,10 +20,12 @@ fn os_name() -> String {
 }
 
 #[tauri::command]
-fn get_processes() -> Vec<ProcessInfo> {
+fn get_processes(sort_by: String, direction: Option<String>) -> Vec<ProcessInfo> {
+    println!("Sorting by: {:?}, Direction: {:?}", sort_by, direction);
     let mut sys = System::new_all();
     sys.refresh_all(); 
 
+    // Collect processes into a vector
     let mut processes: Vec<ProcessInfo> = sys.processes()
         .iter()
         .map(|(pid, process)| {
@@ -37,10 +39,49 @@ fn get_processes() -> Vec<ProcessInfo> {
         })
         .collect();
 
-    processes.sort_by(|a, b| a.name.to_lowercase().cmp(&b.name.to_lowercase()));
-    processes // Sort processes by name
 
+    let is_ascending = direction.unwrap_or_else(|| "ascending".to_string()) == "ascending";
+    
+
+    match sort_by.as_str() { // sort processes based on the specified attribute
+        "name" => {
+            if is_ascending {
+                processes.sort_by(|a, b| a.name.to_lowercase().cmp(&b.name.to_lowercase()));
+            } else {
+                processes.sort_by(|a, b| b.name.to_lowercase().cmp(&a.name.to_lowercase()));
+            }
+        }
+        "pid" => {
+            if is_ascending {
+                processes.sort_by(|a, b| a.pid.cmp(&b.pid));
+            } else {
+                processes.sort_by(|a, b| b.pid.cmp(&a.pid));
+            }
+        }
+        // "cpu_usage" => {
+        //     if is_ascending {
+        //         processes.sort_by(|a, b| a.cpu_usage.partial_cmp(&b.cpu_usage).unwrap_or(std::cmp::Ordering::Equal));
+        //     } else {
+        //         processes.sort_by(|a, b| b.cpu_usage.partial_cmp(&a.cpu_usage).unwrap_or(std::cmp::Ordering::Equal));
+        //     }
+        // }
+        // "status" => {
+        //     if is_ascending {
+        //         processes.sort_by(|a, b| a.status.cmp(&b.status));
+        //     } else {
+        //         processes.sort_by(|a, b| b.status.cmp(&a.status));
+        //     }
+        // }
+        _ => {
+            // Default to name ascending for any unrecognized sort attribute
+            processes.sort_by(|a, b| a.name.to_lowercase().cmp(&b.name.to_lowercase()));
+        }
+    }
+
+    processes
 }
+
+
 
 #[tauri::command]
 fn kill_process(pid: u32) -> bool {
